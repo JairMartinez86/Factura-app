c0c4e600fa8f5fbc6c62e0e7b8a123e98f662fdd
JairMartinez86 <jmartinezg86@gmail.com>
2024-10-21 12:25:43 -0600

    charts color

--- src/app/SHARED/componente/sidebar/sidebar-charts/sidebar-charts.component.ts
+++ src/app/SHARED/componente/sidebar/sidebar-charts/sidebar-charts.component.ts
@@ -251,32 +251,32 @@ export class SidebarChatsComponent {
                 type: 'bar',
                 label: this.Titulo[0],
                 data: this.datos_1,
+                backgroundColor: [
+                  'rgba(255, 69, 0, 0.4)',
+                
+  
+              ],borderColor: [
+                'rgba(255, 69, 0, 1)',
+
+  
+              ],
+              borderWidth: 3
                 
             }, {
                 type: 'bar',
                 label: this.Titulo[1],
                 data: this.datos_2,
                 backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(50, 155, 37, 0.2)'
+                  'rgba(0, 157, 67, 0.4)',
+
   
               ],
               borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                  'rgba(50, 155, 37, 1)'
+                'rgba(0, 157, 67, 1)',
+
   
               ],
-              borderWidth: 1
+              borderWidth: 3
             }],
             labels: this.Meses
         },
