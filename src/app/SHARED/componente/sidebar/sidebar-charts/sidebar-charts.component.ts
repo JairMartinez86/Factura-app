5cd1d0293b02425b7c0b3d5b98fdc7608b2e02d4
JairMartinez86 <jmartinezg86@gmail.com>
2024-10-22 09:01:37 -0600

    charts

--- src/app/SHARED/componente/sidebar/sidebar-charts/sidebar-charts.component.ts
+++ src/app/SHARED/componente/sidebar/sidebar-charts/sidebar-charts.component.ts
@@ -16,22 +16,22 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 @Component({
   selector: 'app-sidebar-charts',
   standalone: true,
-  imports: [ CommonModule, IgxComboModule, ReactiveFormsModule, IgxIconModule],
+  imports: [CommonModule, IgxComboModule, ReactiveFormsModule, IgxIconModule],
   templateUrl: './sidebar-charts.component.html',
   styleUrl: './sidebar-charts.component.scss'
 })
 export class SidebarChatsComponent {
 
-  private datos_1 : number[] = [];
-  private datos_2 : number[] = [];
-  public Titulo : string [] = ["", ""];
-  private Meses : string[] = [];
-  private lstDatos : any[] = [];
-  public Total : number;
-  public Sucursal : string = "";
+  private datos_1: number[] = [];
+  private datos_2: number[] = [];
+  public Titulo: string[] = ["", ""];
+  private Meses: string[] = [];
+  private lstDatos: any[] = [];
+  public Total: number;
+  public Sucursal: string = "";
 
-  private myChart : Chart;
-  
+  private myChart: Chart;
+
 
 
   public val = new Validacion();
@@ -51,17 +51,17 @@ export class SidebarChatsComponent {
   private ctx: Chart;
 
 
-landscape = window.matchMedia("(orientation: landscape)");
-  constructor(private GET : getServidor, private cFunciones: Funciones){
+  landscape = window.matchMedia("(orientation: landscape)");
+  constructor(private GET: getServidor, private cFunciones: Funciones) {
 
 
     this.val.add("cmbBodega", "1", "LEN>=", "0", "", "");
- 
+
 
     this.val.Get("cmbBodega").setValue([]);
 
-    
-   
+
+
 
   }
 
@@ -94,7 +94,7 @@ landscape = window.matchMedia("(orientation: landscape)");
 
 
 
-  
+
 
 
     this.GET.GetCharts().subscribe(
@@ -125,12 +125,12 @@ landscape = window.matchMedia("(orientation: landscape)");
             this.datos_1.splice(0, this.datos_1.length);
             this.datos_2.splice(0, this.datos_2.length);
 
-            
+
             this.cmbBodega.setSelectedItem(this.cFunciones.Bodega);
 
-            
 
-            
+
+
           }
 
         },
@@ -138,7 +138,7 @@ landscape = window.matchMedia("(orientation: landscape)");
           document.getElementById("btn-Charts-Refrescar")?.removeAttribute("disabled");
           this.cFunciones.DIALOG.getDialogById("dialog-wait-charts")?.close();
 
-   
+
 
           if (this.cFunciones.DIALOG.getDialogById("error-servidor") == undefined) {
             this.cFunciones.DIALOG.open(DialogErrorComponent, {
@@ -156,29 +156,32 @@ landscape = window.matchMedia("(orientation: landscape)");
 
   }
 
-  
 
-  private Refrescar_Chart() : void{
 
-  
-    let myClonedObject : any[];
+  private Refrescar_Chart(): void {
 
-    let Bodegas : string[] = this.val.Get("cmbBodega").value;
+
+    let myClonedObject: any[];
+
+    let Bodegas: string[] = this.val.Get("cmbBodega").value;
+    this.Sucursal = "";
 
     var Datos = JSON.parse(JSON.stringify(this.lstDatos));
 
-    if(this.val.Get("cmbBodega").value.length == 0)
-    {
-      myClonedObject  = Object.assign([], Datos); 
+    if (this.val.Get("cmbBodega").value.length == 0) {
+
+      myClonedObject = Object.assign([], Datos);
     }
-    else
-    {
-      myClonedObject  = Object.assign([], Datos.filter((f : any) =>  Bodegas.includes(f.Codigo))); 
+    else {
+      if (this.val.Get("cmbBodega").value.length == 1) this.Sucursal = this.lstBodega.find(f => f.Codigo == this.val.GetValue("cmbBodega"))?.Key!;
+
+
+      myClonedObject = Object.assign([], Datos.filter((f: any) => Bodegas.includes(f.Codigo)));
     }
 
     this.Total = 0;
 
-    myClonedObject.forEach((f : any) => {
+    myClonedObject.forEach((f: any) => {
       f.Codigo = "";
       f.Bodega = "";
       f.Contado = 0;
@@ -191,73 +194,72 @@ landscape = window.matchMedia("(orientation: landscape)");
 
 
 
-    var d1 : any[] = myClonedObject.filter(w => w.Anio == Number(this.Titulo[0])).reduce((acc, item) => {
-  
-      let accItem : any = acc.find( (ai : any) => ai.Anio == item.Anio && ai.Mes == item.Mes)
+    var d1: any[] = myClonedObject.filter(w => w.Anio == Number(this.Titulo[0])).reduce((acc, item) => {
 
-      
-      
-      if(accItem){
-          accItem.VentaNeta += item.VentaNeta 
-      }else{
-         acc.push(item)
+      let accItem: any = acc.find((ai: any) => ai.Anio == item.Anio && ai.Mes == item.Mes)
+
+
+
+      if (accItem) {
+        accItem.VentaNeta += item.VentaNeta
+      } else {
+        acc.push(item)
       }
-      this.Sucursal = item.Codigo + " " + item.Bodega;
+
       return acc;
-    },[]);
+    }, []);
 
 
-   
-    var d2 : any[] = myClonedObject.filter(w => w.Anio == Number(this.Titulo[1])).reduce((acc, item) => {
-  
-      let accItem : any = acc.find( (ai : any) => ai.Anio == item.Anio && ai.Mes == item.Mes)
 
-      
-      
-      if(accItem){
-          accItem.VentaNeta += item.VentaNeta 
-          
-      }else{
-         acc.push(item)
+    var d2: any[] = myClonedObject.filter(w => w.Anio == Number(this.Titulo[1])).reduce((acc, item) => {
+
+      let accItem: any = acc.find((ai: any) => ai.Anio == item.Anio && ai.Mes == item.Mes)
+
+
+
+      if (accItem) {
+        accItem.VentaNeta += item.VentaNeta
+
+      } else {
+        acc.push(item)
       }
-      this.Sucursal = item.Codigo + " " + item.Bodega;
       this.Total += item.VentaNeta;
       return acc;
-    },[]);
+    }, []);
 
 
 
-    d1 = d1.sort((a, b) => {return a.Mes - b.Mes;});
-    d2 = d2.sort((a, b) => {return a.Mes - b.Mes;});
+    d1 = d1.sort((a, b) => { return a.Mes - b.Mes; });
+    d2 = d2.sort((a, b) => { return a.Mes - b.Mes; });
 
- 
+
 
     this.datos_1.splice(0, this.datos_1.length);
-    d1.forEach( f => {
+    d1.forEach(f => {
       this.datos_1.push(f.VentaNeta);
     });
 
     this.datos_2.splice(0, this.datos_2.length);
-    d2.forEach( f => {
+    d2.forEach(f => {
       this.datos_2.push(f.VentaNeta);
     });
 
 
 
     this.Crear_Chart();
-    
 
-    
- 
+
+
+
 
 
 
   }
- 
 
-  
 
-  
+
+
+
   public V_Select_Bodega(event: any) {
 
     this.val.Get("cmbBodega").setValue(event.newValue);
@@ -282,8 +284,8 @@ landscape = window.matchMedia("(orientation: landscape)");
 
 
 
-    this.landscape.addEventListener("change", (ev : any) => {
-     
+    this.landscape.addEventListener("change", (ev: any) => {
+
 
       this.Crear_Chart();
 
@@ -291,72 +293,101 @@ landscape = window.matchMedia("(orientation: landscape)");
 
 
     this.V_CargarCharts();
-   
+
 
   }
 
 
-  private Crear_Chart()
-  {
+  private Crear_Chart() {
 
- 
+
     this.myChart?.destroy();
 
     this.myChart = new Chart("Charts_Venta_Neta", {
       data: {
-          datasets: [{
-              type: 'bar',
-              label: this.Titulo[0],
-              data: this.datos_1,
-              backgroundColor: [
-                'rgba(255, 69, 0, 0.4)',
-              
-
-            ],borderColor: [
-              'rgba(255, 69, 0, 1)',
+        datasets: [{
+          type: 'bar',
+          label: this.Titulo[0],
+          data: this.datos_1,
+          backgroundColor: [
+            'rgba(255, 69, 0, 0.4)',
 
 
-            ],
-            borderWidth: 1,
-              
-          }, {
-              type: 'bar',
-              label: this.Titulo[1],
-              data: this.datos_2,
-              backgroundColor: [
-                'rgba(0, 157, 67, 0.4)',
+          ], borderColor: [
+            'rgba(255, 69, 0, 1)',
 
 
-            ],
-            borderColor: [
-              'rgba(0, 157, 67, 1)',
+          ],
+          borderWidth: 1,
+
+        }, {
+          type: 'bar',
+          label: this.Titulo[1],
+          data: this.datos_2,
+          backgroundColor: [
+            'rgba(0, 157, 67, 0.4)',
 
 
-            ],
-            borderWidth: 1
-          }],
-          labels: this.Meses
+          ],
+          borderColor: [
+            'rgba(0, 157, 67, 1)',
+
+
+          ],
+          borderWidth: 1,
+        }],
+        labels: this.Meses,
       },
       options: {
         responsive: true,
+        maintainAspectRatio: false,
         indexAxis: window.screen.orientation.angle == 90 || navigator.userAgent.includes("Android") ? "y" : "x",
         animation: {
           duration: 1000,
-          onComplete: function() {
-  
-            
+          onComplete: function () {
+
+
           }
         },
-  
+
         scales: {
           x: {
             display: true,
+
           },
           y: {
             beginAtZero: true,
-            display: true,
-  
-            
+            title: {
+              display: true,
+              align: 'center',
+              text: this.Sucursal,
+              color: 'blue',
+              font: {
+                weight: 'bold',
+                size: 15
+              }
+            },
+
+            ticks: {
+
+              callback: function (value: any, index, ticks) {
+
+
+                var v: any = (Math.abs(value) / 1000).toFixed(1);
+
+
+                return value.toLocaleString('en-US', {
+                  // add suffixes for thousands, millions, and billions
+                  // the maximum number of decimal places to use
+                  maximumFractionDigits: 2,
+                  // specify the abbreviations to use for the suffixes
+                  notation: 'compact',
+                  compactDisplay: 'short'
+                });
+
+              }
+            }
+
           }
         },
         plugins: {
@@ -364,20 +395,20 @@ landscape = window.matchMedia("(orientation: landscape)");
             color: 'black',
             anchor: 'end',
             align: 'end',
-            rotation: window.screen.orientation.angle == 0 && !navigator.userAgent.includes("Android") ? -45 : 0, 
-          font: {
-            weight: 'bold',
-            size: 10, 
-          },
-          
-            formatter: function(value : any, context : any) {
-  
-              var v : any = (Math.abs(value)/1000).toFixed(1);
-  
-            //  return Math.abs(value) > 999 ? Math.sign(value)* v + 'k' : Math.sign(value)*Math.abs(value)
-  
-  
-            return  value.toLocaleString('en-US', {
+            rotation: window.screen.orientation.angle == 0 && !navigator.userAgent.includes("Android") ? -45 : 0,
+            font: {
+              weight: 'bold',
+              size: 10,
+            },
+
+            formatter: function (value: any, context: any) {
+
+              var v: any = (Math.abs(value) / 1000).toFixed(1);
+
+              //  return Math.abs(value) > 999 ? Math.sign(value)* v + 'k' : Math.sign(value)*Math.abs(value)
+
+
+              return value.toLocaleString('en-US', {
                 // add suffixes for thousands, millions, and billions
                 // the maximum number of decimal places to use
                 maximumFractionDigits: 2,
@@ -385,21 +416,44 @@ landscape = window.matchMedia("(orientation: landscape)");
                 notation: 'compact',
                 compactDisplay: 'short'
               });
-  
-  
-  
+
+
+
+            }
+          },
+
+          legend:{
+            labels: {
+              font: {
+                size: 15
+              }
+            }
+          },
+
+          title: {
+            display: true,
+            align: 'center',
+            text: 'VENTAS ' + this.Titulo[0] + ' - ' + this.Titulo[0],
+          },
+          subtitle: {
+            display: true,
+            align: "center",
+            text: this.cFunciones.NumFormat(this.Total, "2"),
+            color: "blue",
+            font: {
+              weight: 'bold',
+              size: 15,
             }
           }
-    
+
         }
       }
-  ,
+      ,
       plugins: [ChartDataLabels],
 
-  });
+    });
 
-
-
+    this.myChart.canvas.style.height = "600px";
   }
 
 
